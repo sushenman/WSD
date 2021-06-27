372607b192aabd50e7f6369af4aa8556009ec1ad
sushenman <sushenman83@gmail.com>
2021-06-27 22:12:55 +0545

    html

--- website/contact.js
+++ website/contact.js
@@ -1,17 +1,28 @@
-function  validation(){
-    var name = document.forms["myform"]["Name"];
-    
-    var email = document.forms["myform"]["Email"];
+const toggleButton = document.querySelector(".toggle-button")
+const box = document.querySelector('.box');
 
-if(name.value=""){
-    window.alert("Please enter your name");
-    name.focus();
-    return false;
-}
-if(email.value=""){
-    window.alert("Please enter your email");
-    name.focus();
-    return false;
-}
-return true;
+console.log(toggleButton);
+
+toggleButton.addEventListener('click', ()=>{
+  box.classList.toggle('active');
+})
+
+
+function validation()
+{
+    var name = document.getElementById("Name").value;
+    var email = document.getElementById("Email").value;
+    var errormsg = document.getElementById("errormsg");
+    var text;
+  
+    if (name =="" ){
+     window.alert("Please enter your name");
+      return false;
+    }
+    
+    if(email ==""){
+      window.alert("Please enter your email");
+      return false;
+    }
+
 }
